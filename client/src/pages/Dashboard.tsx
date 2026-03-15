79e454ea9cc9bbbc8dc8de53420d68728b520014
Trash Oracle Studios <j4ck3twashere@protonmail.com>
2026-03-15 12:27:43 -0500

    Update Dashboard.tsx
    Changed the text at the top of the page to fit the vibe of the site better

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -26,10 +26,10 @@ export default function Dashboard() {
         <div className="flex flex-col md:flex-row md:items-end justify-between gap-4 mb-8">
           <div>
             <h1 className="text-3xl md:text-4xl font-display font-bold text-gray-900 dark:text-white">
-              Discover People
+              OwO
             </h1>
             <p className="text-gray-500 dark:text-gray-400 mt-2">
-              Find someone who matches your vibe.
+              Look at all these cutie-patooties! Who's YOUR best match? :3
             </p>
           </div>
           
